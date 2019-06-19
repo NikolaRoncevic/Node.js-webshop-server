d736f744e28e8be9c9f89d3b82577507092aeacd
NikolaRoncevic <nikola.roncevic123@gmail.com>
2019-06-19 13:25:59 +0200

    changed search to filter by predefined values of checkboxes and by string from search bar

--- src/routers/search.js
+++ src/routers/search.js
@@ -7,10 +7,13 @@ var router = new express.Router();
     Search is possible based on producer,ramMemory,processor,
     graphic Card,Name and price
 */
-router.get('/search', (req, res) => {
-    //todo: add search based on price range,price higher than,lower than
+router.get('/search', async(req, res) => {
+    //todo: treba odraditi pretragu baziranu na cenama ali 
+    //za to cemo dodati poseban request koji ce primati listu dokumenata
+    //i onda je filtrirati po ceni manje vece
     let { producer, ramMemory, processor, graphicCard, search } = req.query;
     let query = {};
+    let searchArray
     if (producer)
         query.producer = producer.split('%')
     if (ramMemory)
@@ -19,14 +22,47 @@ router.get('/search', (req, res) => {
         query.processor = processor.split('%')
     if (graphicCard)
         query.graphicCard = graphicCard.split('%')
-        /* Phone.find(query).then((data) => {
+    if (search) {
+        searchArray = search.toLowerCase().split('%')
 
-            res.status(200).send(JSON.stringify(data))
+    }
+    let filteredBySearch = []
+    let products
+    try {
+        products = await Phone.find(query)
 
-        }).catch((e) => {
-            res.status(400).send('Bad request')
-            console.log(e)
-        }) */
+    } catch (e) {
+        res.status(500).send()
+    }
+
+    if (searchArray) {
+        for (product of products) {
+            if (searchArray.includes(product.name.toLowerCase())) {
+                filteredBySearch.push(product)
+                continue
+            }
+            //zakomentarisano zato sto nisu potpuni podaci
+            /* if (searchArray.includes(product.screenSize.toString().toLowerCase())) {
+                filteredBySearch.push(product)
+                continue
+            }
+            if (searchArray.includes(product.ramMemory.toLowerCase())) {
+                filteredBySearch.push(product)
+                continue
+            }*/
+            if (searchArray.includes(product.producer.toLowerCase())) {
+                filteredBySearch.push(product)
+                continue
+            }
+        }
+    } else {
+        console.log('doso da ispisem bez searcha')
+        return res.status(200).send(JSON.stringify(products))
+    }
+    if (filteredBySearch.length === 0) {
+        return res.status(400).send()
+    }
+    res.status(200).send(filteredBySearch)
 })
 
 module.exports = router
